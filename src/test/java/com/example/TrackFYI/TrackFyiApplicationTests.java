0471012dea17b6c274d7fecc50e37bfd09c79ed7
Tariq Elamin <tariqelamin@Tariqs-MacBook-Air-7.local>
2026-03-12 11:28:14 -0700

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/TrackFYI/TrackFyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrackFYI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
