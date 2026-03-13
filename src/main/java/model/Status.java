5f2f002b43efae09f3a99d718aa4a9099df94a28
Tariq Elamin <tariqelamin@Tariqs-MacBook-Air-7.local>
2026-03-12 18:22:20 -0700

    add approprite single application model

---
+++ src/main/java/model/Status.java
@@ -0,0 +1,12 @@
+package model;
+
+public enum Status {
+    REJECTED,
+    APPLIED,
+    GHOSTED,
+    ASSESSMENT_SENT,
+    PHONE_SCREEN,
+    BEHAVIORAL_ROUND,
+    TECHNICAL_ROUND,
+    OFFER
+}
